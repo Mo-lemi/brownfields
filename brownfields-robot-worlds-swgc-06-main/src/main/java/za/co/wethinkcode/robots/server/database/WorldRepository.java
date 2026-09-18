7dd46f03abaee45b1d544562953803881da76da7
Molemi Molokoane <iimillicent02@gmail.com>
2026-09-18 15:04:53 +0200

    added a recent repo.

--- brownfields-robot-worlds-swgc-06-main/src/main/java/za/co/wethinkcode/robots/server/database/WorldRepository.java
+++
@@ -1,9 +0,0 @@
-package za.co.wethinkcode.robots.server.database;
-
-import za.co.wethinkcode.robots.server.World;
-
-public interface WorldRepository {
-    boolean worldExists(String name);
-    void saveWorld(String name, World world);
-    World loadWorld(String name);
-}
